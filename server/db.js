e2740deacdb6e6b3cdce02892868b2cb0627cf7d
Shiyuan M2 MAX <theolin@yahoo.com>
2023-08-24 21:31:48 -0400

    Fix database inactive timeout

--- server/db.js
+++ server/db.js
@@ -1,8 +1,27 @@
 import mysql from 'mysql2';
 
-export const db = mysql.createConnection({
+export const db = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: '0128Zhu!',
-  database: 'react_blog'
+  database: 'react_blog',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
+
+// Handle connection errors
+db.on('connection', connection => {
+  connection.on('error', err => {
+    console.error('Database connection error:', err);
+  });
+});
+
+const oneMinute = 60000;
+setInterval(() => {
+  db.query('SELECT 1', (err, results) => {
+    if (err) {
+      console.error('Keep-alive query error:', err);
+    }
+  });
+}, oneMinute * 60);
